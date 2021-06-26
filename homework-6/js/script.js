491af32c52eb5dcd3f41a7162bd3308c9ecb8ceb
Vitalii <vitaliimelnykov@gmail.com>
2021-06-26 19:43:34 +0300

    finished

--- homework-6/js/script.js
+++ homework-6/js/script.js
@@ -30,32 +30,22 @@ const students = [
 
 // 1.Створіть функцію getSubjects(students[0] --> ["Math", "Algorithms", "Data science"] - яка повертає список предметів для конкретного студента.
 // Зверніть увагу – назву предмету необхідно повертати з великої літери, а _ – замінити на пробіл
-
 function getSubjects(name) {
 	const student = students.find((student) => student.name === name);
 
-	if (student) {
-		const studentSubjects = Object.keys(student.subjects);
-		const updatedSubjects = studentSubjects.map((subject) => {
-			const subjectUpperCased = subject[0].toUpperCase() + subject.slice(1);
-			return subjectUpperCased.replace("_", "");
-		});
+	const studentSubjects = Object.keys(student.subjects);
+	const updatedSubjects = studentSubjects.map((subject) => {
+		const subjectUpperCased = subject[0].toUpperCase() + subject.slice(1);
+		return subjectUpperCased.replace("_", "");
+	});
 
-		return updatedSubjects;
-	}
-
-	throw new Error("Помика!");
+	return updatedSubjects;
 }
 
-try {
-	console.log(getSubjects("Anton"));
-} catch (error) {
-	console.log(error.message);
-}
+console.log(getSubjects("Anton"));
 
 // Створіть функцію getAverageMark(students[0]) --> 3.79 – яка поверне середню оцінку по усім предметам для переданого студента НЕ МАСИВА СТУДЕНТІВ.
 // Оцінку округліть до 2ого знаку. Можна використовувати функції, написані у попередніх домашніх завданнях :)
-
 function average(array) {
 	const total = array.reduce((prev, next) => prev + next, 0);
 	const averageMark = Number((total / array.length).toFixed(2));
@@ -71,9 +61,11 @@ function getAverageMark(name) {
 		[]
 	);
 	const avarageMark = average(arrayStudentMarksSubjects);
+
 	return avarageMark;
 }
-// console.log(getAverageMark("Anton"));
+
+console.log(getAverageMark("Anton"));
 
 // 3.Створіть функцію getStudentInfo(students[0]) --> { "course": 3, "name": "Tanya", "averageMark": 3.79} – яка повертає інформацію загального виду по переданому студенту
 //  (вам знадобиться функція з попереднього завдання). ПОвинна бути виведена інформація: курс, ім'я, середня оцінка.
@@ -86,11 +78,11 @@ function getStudentInfo(name) {
 		averageMark: getAverageMark(name),
 	};
 }
-// console.log(getStudentInfo("Tanya"));
+
+console.log(getStudentInfo("Tanya"));
 
 // 4.Ствроіть функцію getStudentsNames(students) --> ["Anton", "Tanya, "Victor"] –
 // яка повертає імена студентів у алфавітному порядку.
-
 function getStudentsNames(students) {
 	const arrayNamesStudent = students.map((student) => student.name).sort();
 
@@ -99,18 +91,35 @@ function getStudentsNames(students) {
 
 console.log(getStudentsNames(students));
 
-// console.log(Object.values(students[0].name));
-// console.log(students);
 // 5.Створіть функцію getBestStudent(students) --> "Anton" – яка повертає кращого студента зі списку по показнику середньої оцінки.
 function getBestStudent(students) {
-	const sort = Object.values(students.subject);
-	return sort;
+	const allAvarageMarksStudents = students.map((student) => {
+		return getAverageMark(student.name);
+	});
+
+	const maxMark = Math.max(...allAvarageMarksStudents);
+	const bestStudent = [];
+
+	students.forEach((student) => {
+		if (getAverageMark(student.name) === maxMark) {
+			bestStudent.push(student.name);
+		}
+	});
+
+	return bestStudent;
+}
+console.log(getBestStudent(students));
+
+// 6.Створіть функцію calculateWordLetters("тест") --> { "т": 2, "е": 1, "с": 1 } – яка повертає обє'кт, в якому ключі це букви у слові, а значення – кількість їх повторень.
+function calculateWordLetters(string) {
+	const arrayFromString = string.split("");
+
+	const countLetters = arrayFromString.reduce((lettersObj, letter) => {
+		lettersObj[letter] ? lettersObj[letter]++ : (lettersObj[letter] = 1);
+		return lettersObj;
+	}, {});
+
+	return countLetters;
 }
 
-// console.log(getBestStudent(students));
-
-// console.log(Object.values(students[0]));
-const student = students.find((student) => student.name === "Anton");
-const studentMarksSubjects = Object.values(student.subjects);
-console.log(student);
-console.log(studentMarksSubjects);
+console.log(calculateWordLetters("тест"));
