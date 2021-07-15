883b9f13e5312059d7476839a42be2d08a251acc
Vitalii Melnykov <vitaliimelnykov@gmail.com>
2021-07-15 12:10:11 +0300

    delete console

--- homework-11/js/view.js
+++ homework-11/js/view.js
@@ -3,7 +3,6 @@ const button = document.querySelector(".button");
 const input = document.getElementById("input");
 
 button.addEventListener("click", () => {
-  console.log(input.value);
   getRandomChinese(input.value).then((result) => {
     output.textContent = result;
   });
