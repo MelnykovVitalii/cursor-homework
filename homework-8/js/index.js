78fe7cb3d130807fc30e889af2f80ea7006907bd
Vitalii <vitaliimelnykov@gmail.com>
2021-07-17 15:18:56 +0300

    after refacring

--- homework-8/js/index.js
+++ homework-8/js/index.js
@@ -11,71 +11,55 @@ class Student {
 
 	getInfo() {
 		if (this.isActiveStudent) {
-			console.log(
-				`${this.fullName}, студент ${this.university} університету ${this.course} курсу`
-			);
-		} else {
-			console.log(
-				`${this.fullName}, студент ${this.university} університету виключений з ${this.course} курсу`
-			);
+			return `${this.fullName}, студент ${this.university} університету ${this.course} курсу`;
 		}
+		return `${this.fullName}, студент ${this.university} університету виключений з ${this.course} курсу!`;
 	}
 
 	get getMarks() {
 		if (this.isActiveStudent) {
-			console.log(`Оцінки студента: ${this.marks}`);
-		} else {
-			console.log("Студент виключений з курсу");
+			return this.marks;
 		}
+		return null;
 	}
 
 	set setMarks(mark) {
 		if (this.isActiveStudent) {
 			this.marks.push(mark);
-			console.log(`Була додана оцінка ${mark}`);
-		} else {
-			console.log("Студент виключений, неможливо додати оцінку!");
 		}
 	}
 
 	get getAverageMark() {
-		let averageMark = 0;
 		if (this.marks.length === 0) {
-			console.log(`Середня оцінка студента: ${averageMark}`);
-		} else {
-			averageMark =
-				this.marks.reduce((total, mark) => total + mark) / this.marks.length;
-			console.log(`Середня оцінка студента: ${averageMark.toFixed(1)}`);
+			return 0;
 		}
-		return Number(averageMark);
+
+		const marksTotal = this.marks.reduce((total, mark) => total + mark);
+
+		return Math.round(marksTotal / this.marks.length);
 	}
 
 	dismiss() {
 		this.isActiveStudent = false;
-		console.log(`Студент ${this.fullName} виключений з курсу!`);
 	}
 
 	recover() {
 		this.isActiveStudent = true;
-		console.log(`Студент ${this.fullName} поновлений на курс!`);
 	}
 }
 
 const anton = new Student("ОНПУ", 5, "Антон Рудько");
 
-anton.getInfo();
+console.log(anton.getInfo());
 anton.setMarks = 5;
 anton.setMarks = 4;
 anton.setMarks = 3;
-anton.getMarks;
-anton.getAverageMark;
+console.log("Оцінки студента", anton.getMarks);
+console.log("Середня оцінка студента:", anton.getAverageMark);
 anton.dismiss();
-anton.setMarks = 5;
-anton.recover();
-anton.setMarks = 4;
-anton.getMarks;
-anton.setMarks = 5;
-anton.getMarks;
+console.log(anton.getInfo());
+anton.setMarks = 3;
+console.log(anton.getMarks);
 
 // Advanced
 
@@ -91,17 +75,21 @@ class BudgetStudent extends Student {
 		if (this.getAverageMark >= 4 && this.isActiveStudent) {
 			console.log(`Ваша стипендія 1400 грн!`);
 		} else {
-			console.log("Ви не отримуєте стипендії!");
-			this.isActiveStudent
-				? console.log(`Ваш середній бал: ${this.getAverageMark}.`)
-				: console.log("Ви відраховані!");
+			const reasonInfo = this.isActiveStudent
+				? `Ваш середній бал: ${this.getAverageMark}.`
+				: "Ви відраховані!";
+			console.log(`Ви не отримуєте стипендії! ${reasonInfo}`);
 		}
 	}
 }
 
 console.log("Студент бюджетник!");
 const kira = new BudgetStudent("ОНУ ім. Мечникова", 4, "Кіра Петрова");
-kira.setMarks = 5;
-kira.setMarks = 3;
+
+console.log(kira.getInfo());
+console.log("Встановлена оцінка:", (kira.setMarks = 4));
+console.log("Встановлена оцінка:", (kira.setMarks = 2));
 kira.dismiss();
+console.log(kira.getInfo());
 kira.recover();
+console.log(`${kira.getInfo()} поновлений!`);
