2b1d227f6872b61c7eb8d0f03c5b0d0a97589ea5
Vitalii <vitaliimelnykov@gmail.com>
2021-08-12 22:27:48 +0300

    after fix

--- homework-11/js/script.js
+++ homework-11/js/script.js
@@ -1,20 +1,20 @@
 async function getRandomChinese(length) {
-  const message = "Введіть число більше 0";
-  let chinaString = "";
+	const message = 'Введіть число більше 0';
+	let chinaString = '';
 
-  if (!length || length <= 0) return message;
+	if (!length || length <= 0) return message;
 
-  while (chinaString.length < length) {
-    const promise = new Promise((resolve) => {
-      setTimeout(() => {
-        const string = Date.now().toString().slice(-5);
-        const chinaCharacter = String.fromCharCode(string);
-        resolve(chinaCharacter);
-      }, 50);
-    });
+	for (let i = 0; i < length; i++) {
+		const promise = new Promise((resolve) => {
+			setTimeout(() => {
+				const string = Date.now().toString().slice(-5);
+				const chinaCharacter = String.fromCharCode(string);
+				resolve(chinaCharacter);
+			}, i * 50);
+		});
 
-    chinaString += await promise;
-  }
+		chinaString += await promise;
+	}
 
-  return chinaString;
+	return chinaString;
 }
